0be570d93087175ad86037086515613751139c3f
Tigabu Mitiku <mittig1274@gmail.com>
2024-12-08 09:13:10 +0300

    test git

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      {/* Test */}
+      {/* Test Git*/}
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
